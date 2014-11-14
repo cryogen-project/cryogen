28411e7236b07dc7e60a6830536a6809fac14922
Chris Gill <chrisfgill@gmail.com>
2014-11-14 11:58:02 -0500

    updated with git src pull

--- src/leiningen/new/cryogen/src/cryogen/github.clj
+++ src/leiningen/new/cryogen/src/cryogen/github.clj
@@ -1,5 +1,6 @@
 (ns cryogen.github
-  (:require [cheshire.core :as json]))
+  (:require [cheshire.core :as json])
+  (:import (org.apache.commons.codec.binary Base64 StringUtils)))
 
 (defn get-gist [gist-uri]
   (let [gist-id (last (clojure.string/split gist-uri #"/+"))
@@ -17,5 +18,18 @@
          :name (get gist "filename")
          :id gist-id}))))
 
-(get-gist "https://gist.github.com/viperscape/cec68f0791687f5959f1")
+(defn get-src [git-file]
+  (let [gist-resp (try (slurp git-file)
+                       (catch Exception e {:error (.getMessage e)}))]
+    (when-not (:error gist-resp)
+      (if-let [git-src (json/parse-string gist-resp)]
+        {:content (String. (Base64/decodeBase64 (get git-src "content")) "UTF-8")
+         :name (get git-src "name")
+         :uri (get (get git-src "_links") "html")}))))
 
+
+(defn get-gits-ex []
+  [(get-gist "https://gist.github.com/viperscape/cec68f0791687f5959f1")
+   (get-src "https://api.github.com/repos/viperscape/rust-irc/contents/src/main.rs")])
+
+;(prn (get-gits-ex))
