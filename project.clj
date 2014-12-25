64412ec59fa9916e22395ade70a61a6ef62c767c
lacarmen <carmen.la@mail.utoronto.ca>
2014-12-25 12:13:37 -0500

    Bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.6"
+(defproject cryogen/lein-template "0.2.7"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/lacarmen/cryogen"
   :license {:name "Eclipse Public License"
