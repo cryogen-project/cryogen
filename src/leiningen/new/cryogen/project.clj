ec453159fa5565a9df4e107a0b9244a95af16c1f
J Irving <j@lollyshouse.ca>
2014-12-27 02:48:43 -0500

    Add support for :ignored-files config key
    Prevent the compiler from attempting to process files defined by a list
    of regexps. By default ignore emacs and vi backup files.

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen "0.1.0"
+(defproject cryogen "0.1.1-SNAPSHOT"
             :description "Simple static site generator"
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                            [ring/ring-devel "1.3.2"]
                            [compojure "1.3.1"]
                            [ring-server "0.3.1"]
-                           [cryogen-core "0.1.8"]]
+                           [cryogen-core "0.1.9-SNAPSHOT"]]
             :plugins [[lein-ring "0.8.13"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
