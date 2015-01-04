0266e2696311e87a635d0f7ca6de9527074a1642
lacarmen <carmen.la@mail.utoronto.ca>
2015-01-03 20:36:00 -0500

    Added :rss-filters tag for curated RSS feed generation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.11"
+(defproject cryogen/lein-template "0.2.12"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/lacarmen/cryogen"
   :license {:name "Eclipse Public License"
