e9d6337efe86a16c09985475ca653f89992ca844
Yogthos <dmitri.sotnikov@uhn.on.ca>
2015-01-13 14:18:53 -0500

    renamed sample posts to use the new date format

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.14"
+(defproject cryogen/lein-template "0.2.15"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/lacarmen/cryogen"
   :license {:name "Eclipse Public License"
