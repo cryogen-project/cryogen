281669798173e2863413c5b6d0b5e92fff3a74fb
Yogthos <dmitri.sotnikov@gmail.com>
2015-01-17 15:33:04 -0500

    bumped up dependencies

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -7,8 +7,8 @@
                            [ring/ring-devel "1.3.2"]
                            [compojure "1.3.1"]
                            [ring-server "0.3.1"]
-                           [cryogen-markdown "0.1.0"]
-                           [cryogen-core "0.1.16"]]
+                           [cryogen-markdown "0.1.1"]
+                           [cryogen-core "0.1.17"]]
             :plugins [[lein-ring "0.8.13"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
