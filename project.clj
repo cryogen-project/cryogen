03fc8dcaa8f2547f160a75b370a8e2cff6ca651b
lacarmen <carmen.la@mail.utoronto.ca>
2015-01-28 12:30:29 -0500

    Updated docs posts and bumped up versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.18"
+(defproject cryogen/lein-template "0.2.19"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/lacarmen/cryogen"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -8,7 +8,7 @@
                            [compojure "1.3.1"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.1"]
-                           [cryogen-core "0.1.19"]]
+                           [cryogen-core "0.1.20"]]
             :plugins [[lein-ring "0.8.13"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
