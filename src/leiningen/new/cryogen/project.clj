11819240f27f10f467ff2db542f7b5456e373206
Carmen La <carmen.la@mail.utoronto.ca>
2015-03-15 21:52:01 -0400

    Updated dependencies

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -5,7 +5,7 @@
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [ring/ring-devel "1.3.2"]
-                           [compojure "1.3.1"]
+                           [compojure "1.3.2"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.1"]
                            [cryogen-core "0.1.20"]]
