8c01fa15c33a1a16a8f5aefd3dcbb365848235ec
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-04-03 12:49:07 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.20"
+(defproject cryogen/lein-template "0.2.21"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/lacarmen/cryogen"
   :license {:name "Eclipse Public License"
