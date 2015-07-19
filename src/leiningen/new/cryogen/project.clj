43f556f7de124957c1b97757d2efe4f0a2bafb90
Carmen La <carmen.la@mail.utoronto.ca>
2015-07-19 10:49:37 -0400

    bumped up dependencies

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -3,9 +3,9 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
-                           [ring/ring-devel "1.3.2"]
-                           [compojure "1.3.4"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
+                           [ring/ring-devel "1.4.0"]
+                           [compojure "1.4.0"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.1"]
                            [cryogen-core "0.1.24"]]
