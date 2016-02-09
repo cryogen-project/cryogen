6b56d41f09922ac5196cbacaed9c67c5740a668d
Carmen La <carmen.wla@gmail.com>
2016-02-09 15:10:41 -0500

    Bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.34"
+(defproject cryogen/lein-template "0.2.35"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/cryogen-project/cryogen"
   :license {:name "Eclipse Public License"
