cea7396f4f4657fda051c038fb8fa12b94d99a70
Tom L <tomrlu@gmail.com>
2016-03-01 05:20:39 -0600

    Serve `index.html` from subdirs when `clean-urls?` true.
    Also redirects / to /blog-prefix/ when true, /blog-prefix/index.html
    otherwise.

--- src/leiningen/new/cryogen/src/cryogen/server.clj
+++ src/leiningen/new/cryogen/src/cryogen/server.clj
@@ -1,10 +1,12 @@
 (ns cryogen.server
   (:require [compojure.core :refer [GET defroutes]]
             [compojure.route :as route]
-            [ring.util.response :refer [redirect]]
+            [ring.util.response :refer [redirect resource-response]]
+            [ring.util.codec :refer [url-decode]]
             [cryogen-core.watcher :refer [start-watcher!]]
             [cryogen-core.plugins :refer [load-plugins]]
-            [cryogen-core.compiler :refer [compile-assets-timed read-config]]))
+            [cryogen-core.compiler :refer [compile-assets-timed read-config]]
+            [cryogen-core.io :refer [path]]))
 
 (defn init []
   (load-plugins)
@@ -12,7 +14,19 @@
   (let [ignored-files (-> (read-config) :ignored-files)]
     (start-watcher! "resources/templates" ignored-files compile-assets-timed)))
 
+(defn wrap-subdirectories
+  [handler]
+  (fn [request]
+    (let [req-uri (.substring (url-decode (:uri request)) 1)
+          res-path (path req-uri (when (:clean-urls? (read-config)) "index.html"))]
+      (or (resource-response res-path {:root "public"})
+          (handler request)))))
+
 (defroutes handler
-  (GET "/" [] (redirect (str (:blog-prefix (read-config)) "/index.html")))
+  (GET "/" [] (redirect (let [config (read-config)]
+                          (path (:blog-prefix (read-config)) "/"
+                                (when-not (:clean-urls? config) "index.html")))))
   (route/resources "/")
   (route/not-found "Page not found"))
+
+(def app (wrap-subdirectories handler))
