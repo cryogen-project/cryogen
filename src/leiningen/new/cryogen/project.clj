81cdc7c1c020e8eb949c825e4ef5e12e36f88790
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-05-02 00:42:12 -0400

    Update project.clj

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -7,7 +7,7 @@
                            [ring/ring-devel "1.4.0"]
                            [compojure "1.5.0"]
                            [ring-server "0.4.0"]
-                           [cryogen-markdown "0.1.3"]
+                           [cryogen-markdown "0.1.4"]
                            [cryogen-core "0.1.40"]]
             :plugins [[lein-ring "0.9.7"]]
             :main cryogen.core
