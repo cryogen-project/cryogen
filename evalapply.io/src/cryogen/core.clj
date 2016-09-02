4fa032a8c92fa949e6f48bd3f62846f562c10f0a
Erwin Rooijakkers <ErwinRooijakkers@gmail.com>
2016-09-02 23:52:10 +0200

    Remove evalapply

--- evalapply.io/src/cryogen/core.clj
+++
@@ -1,8 +0,0 @@
-(ns cryogen.core
-  (:require [cryogen-core.compiler :refer [compile-assets-timed]]
-            [cryogen-core.plugins :refer [load-plugins]]))
-
-(defn -main []
-  (load-plugins)
-  (compile-assets-timed)
-  (System/exit 0))
