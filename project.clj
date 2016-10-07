5485483bbebc772f98004a45a59e5bc09e9801ee
Carmen La <carmen.wla@gmail.com>
2016-10-06 20:18:24 -0400

    Update highlight.js

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.46"
+(defproject cryogen/lein-template "0.2.47"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/cryogen-project/cryogen"
   :license {:name "Eclipse Public License"
