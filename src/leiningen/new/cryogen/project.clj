b7bc4b8d5346797fee0e0b4583ccfc580f55de22
Carmen La <carmen.wla@gmail.com>
2016-12-04 01:23:19 -0500

    bump versions

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -8,7 +8,7 @@
                            [compojure "1.5.1"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.4"]
-                           [cryogen-core "0.1.42"]]
+                           [cryogen-core "0.1.43"]]
             :plugins [[lein-ring "0.9.7"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
