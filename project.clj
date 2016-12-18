1bde1b1cc7e56e731737d8c04d76edf163b7d648
Carmen La <carmen.wla@gmail.com>
2016-12-18 14:36:55 -0500

    Add :hide-future-posts? key to the config, bump version again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.2.49"
+(defproject cryogen/lein-template "0.2.50"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/cryogen-project/cryogen"
   :license {:name "Eclipse Public License"
