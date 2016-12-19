fcb0069dd42519e9a8f60fb39ae22bf2ccccd793
Carmen La <carmen.wla@gmail.com>
2016-12-18 19:23:32 -0500

    update cryogen-core

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -8,7 +8,7 @@
                            [compojure "1.5.1"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.4"]
-                           [cryogen-core "0.1.45"]]
+                           [cryogen-core "0.1.46"]]
             :plugins [[lein-ring "0.9.7"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
