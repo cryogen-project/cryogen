232ddf9998d657fd153ae967e7f162ff76c08140
Carmen La <carmen.wla@gmail.com>
2017-06-09 22:09:12 -0400

    version 0.2.60

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -4,11 +4,11 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
-                           [ring/ring-devel "1.5.1"]
-                           [compojure "1.5.2"]
+                           [ring/ring-devel "1.6.1"]
+                           [compojure "1.6.0"]
                            [ring-server "0.4.0"]
                            [cryogen-markdown "0.1.6"]
-                           [cryogen-core "0.1.55"]]
+                           [cryogen-core "0.1.56"]]
             :plugins [[lein-ring "0.9.7"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
