324e87f11565088a6660f736405c77ffd4c29df9
Sotnikov, Dmitri <dmitri.sotnikov@uhn.ca>
2019-03-05 15:29:30 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
-(defproject cryogen/lein-template "0.3.7"
+(defproject cryogen/lein-template "0.3.8"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/cryogen-project/cryogen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/cryogen-project/cryogen.git"}
-  :dependencies [[leinjacker "0.4.2"]]
+  :dependencies [[org.clojure/core.unify "0.5.7"]
+                 [org.clojure/core.contracts "0.0.6"]
+                 [leinjacker "0.4.2"
+                  :exclusions [org.clojure/clojure
+                               org.clojure/core.contracts
+                               org.clojure/core.unify]]
+                 [org.clojure/tools.namespace "0.2.11"
+                  :exclusions [org.clojure/clojure]]]
   :eval-in-leiningen true)
--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -3,13 +3,13 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.9.0"]
+            :dependencies [[org.clojure/clojure "1.10.0"]
                            [ring/ring-devel "1.7.1"]
                            [compojure "1.6.1"]
                            [ring-server "0.5.0"]
                            [cryogen-markdown "0.1.7"]
-                           [cryogen-core "0.1.65"]]
-            :plugins [[lein-ring "0.9.7"]]
+                           [cryogen-core "0.1.66"]]
+            :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
                    :handler cryogen.server/handler})
