ce78be7a6cc7040a62fa7c7169d49ef87b654944
Dominic Freeston <dominic.freeston@gmail.com>
2019-06-15 14:35:05 +0100

    Update server.clj to match new structure

--- src/leiningen/new/cryogen/src/cryogen/server.clj
+++ src/leiningen/new/cryogen/src/cryogen/server.clj
@@ -1,23 +1,25 @@
 (ns cryogen.server
   (:require [compojure.core :refer [GET defroutes]]
             [compojure.route :as route]
-            [ring.util.response :refer [redirect resource-response]]
+            [ring.util.response :refer [redirect file-response]]
             [ring.util.codec :refer [url-decode]]
             [cryogen-core.watcher :refer [start-watcher!]]
             [cryogen-core.plugins :refer [load-plugins]]
-            [cryogen-core.compiler :refer [compile-assets-timed read-config]]
+            [cryogen-core.compiler :refer [compile-assets-timed]]
+            [cryogen-core.config :refer [resolve-config]]
             [cryogen-core.io :refer [path]]))
 
 (defn init []
   (load-plugins)
   (compile-assets-timed)
-  (let [ignored-files (-> (read-config) :ignored-files)]
-    (start-watcher! "resources/templates" ignored-files compile-assets-timed)))
+  (let [ignored-files (-> (resolve-config) :ignored-files)]
+    (start-watcher! "content" ignored-files compile-assets-timed)
+    (start-watcher! "themes" ignored-files compile-assets-timed)))
 
 (defn wrap-subdirectories
   [handler]
   (fn [request]
-    (let [{:keys [clean-urls blog-prefix]} (read-config)
+    (let [{:keys [clean-urls blog-prefix public-dest]} (resolve-config)
           req-uri (.substring (url-decode (:uri request)) 1)
           res-path (condp = clean-urls
                      :trailing-slash (path req-uri "index.html")
@@ -28,15 +30,15 @@
                                           (path req-uri "index.html")
                                           (path (str req-uri ".html")))
                      :dirty (path (str req-uri ".html")))]
-      (or (resource-response res-path {:root "public"})
+      (or (file-response res-path {:root public-dest})
           (handler request)))))
 
 (defroutes routes
-  (GET "/" [] (redirect (let [config (read-config)]
+  (GET "/" [] (redirect (let [config (resolve-config)]
                           (path (:blog-prefix config)
                                 (when (= (:clean-urls config) :dirty)
                                   "index.html")))))
-  (route/resources "/")
+  (route/files "/")
   (route/not-found "Page not found"))
 
 (def handler (wrap-subdirectories routes))
