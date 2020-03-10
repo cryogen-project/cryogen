3e51abf0e8e2db872596118d3962f71bddacbc59
Carmen La <carmen.wla@gmail.com>
2020-03-10 17:24:20 -0400

    bump cryogen-flexmark

--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -7,7 +7,7 @@
                            [ring/ring-devel "1.7.1"]
                            [compojure "1.6.1"]
                            [ring-server "0.5.0"]
-                           [cryogen-flexmark "0.1.0"]
+                           [cryogen-flexmark "0.1.2"]
                            [cryogen-core "0.3.1"]]
             :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
