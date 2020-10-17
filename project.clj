cbd6dfe7bf9f236598563c4074ae694abe29f57e
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-10-16 23:58:05 -0400

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.6.1"
+(defproject cryogen/lein-template "0.6.2"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/cryogen-project/cryogen"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/cryogen/project.clj
+++ src/leiningen/new/cryogen/project.clj
@@ -3,12 +3,12 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.10.0"]
-                           [ring/ring-devel "1.7.1"]
-                           [compojure "1.6.1"]
+            :dependencies [[org.clojure/clojure "1.10.1"]
+                           [ring/ring-devel "1.8.2"]
+                           [compojure "1.6.2"]
                            [ring-server "0.5.0"]
-                           [cryogen-flexmark "0.1.2"]
-                           [cryogen-core "0.3.1"]]
+                           [cryogen-flexmark "0.1.4"]
+                           [cryogen-core "0.3.2"]]
             :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
