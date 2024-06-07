91db53800275311850bddf936f21573c031c326e
Yogthos <yogthos@gmail.com>
2024-06-07 13:07:13 -0400

    bumped up cryogen-core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen/lein-template "0.7.4"
+(defproject cryogen/lein-template "0.7.5"
   :description "A Leiningen template for the Cryogen static site generator"
   :url "https://github.com/cryogen-project/cryogen"
   :license {:name "Eclipse Public License"
