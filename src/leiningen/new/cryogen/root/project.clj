a90845273db4cb9e1c3d49d60618a61edbe26a93
Yogthos <yogthos@gmail.com>
2026-05-31 22:58:24 -0400

    Add managed-dependencies to template and bump to 0.7.8
    - Add :managed-dependencies to pin bouncycastle versions to 1.81.1
      to suppress version range warnings from pantomime 3.1.0
    - Bump lein-template version to 0.7.8

--- src/leiningen/new/cryogen/root/project.clj
+++ src/leiningen/new/cryogen/root/project.clj
@@ -1,17 +1,20 @@
 (defproject cryogen "0.1.0"
-            :description "Simple static site generator"
-            :url "https://github.com/cryogen-project/cryogen"
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.11.3"]
-                           [ring/ring-devel "1.12.1"]
-                           [compojure "1.7.1"]
-                           [ring-server "0.5.0"]
-                           [cryogen-flexmark "0.1.5"]
-                           [cryogen-core "0.4.9"]]
-            :plugins [[lein-ring "0.12.5"]]
-            :main cryogen.core
-            :ring {:init cryogen.server/init
-                   :handler cryogen.server/handler}
-            :aliases {"serve"      ["run" "-m" "cryogen.server"]
-                      "serve:fast" ["run" "-m" "cryogen.server" "fast"]})
+             :description "Simple static site generator"
+             :url "https://github.com/cryogen-project/cryogen"
+             :license {:name "Eclipse Public License"
+                       :url "http://www.eclipse.org/legal/epl-v10.html"}
+             :dependencies [[org.clojure/clojure "1.11.3"]
+                            [ring/ring-devel "1.12.1"]
+                            [compojure "1.7.1"]
+                            [ring-server "0.5.0"]
+                            [cryogen-flexmark "0.1.5"]
+                            [cryogen-core "0.4.9"]]
+             :plugins [[lein-ring "0.12.5"]]
+             :managed-dependencies [[org.bouncycastle/bcpkix-jdk18on "1.81.1"]
+                                    [org.bouncycastle/bcutil-jdk18on "1.81.1"]
+                                    [org.bouncycastle/bcprov-jdk18on "1.81.1"]]
+             :main cryogen.core
+             :ring {:init cryogen.server/init
+                    :handler cryogen.server/handler}
+             :aliases {"serve"      ["run" "-m" "cryogen.server"]
+                       "serve:fast" ["run" "-m" "cryogen.server" "fast"]})
